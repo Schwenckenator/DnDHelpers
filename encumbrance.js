d292e68a3fc59f4e0c8ef1eadea2723885d7db72
Matthew Schwencke <matt.schwencke@gmail.com>
2020-10-05 06:25:55 +0900

    Added Table

--- encumbrance.js
+++ encumbrance.js
@@ -1,4 +1,44 @@
-var itemRow = '<div class="row"><div class="col"><div class="label">Equip</div><div style="margin: 5px; line-height: 30px; vertical-align: middle;"><input class="input short" type="checkbox" ></div></div><div class="col"><div class="label">Qty.</div><input class="input short" type="text"></div><div class="col"><div class="label">Item Name</div><input class="input" type="text"></div><div class="col"><div class="label">Bulk</div><select class="input"><option value="0.01">Tiny (100 to 1)</option><option value="0.1">Small (10 to 1)</option><option value="1">Normal (1)</option><option value="2">Weighty (2)</option><option value="5">Massive (5)</option></select></div><div class="col"><div class="label">Total Bulk</div><input class="input short" type="text" readonly></div></div></div>'
+var itemRow;
+
+var tableList = [];
+
+function initRow(){
+    itemRow += '<div class="trow">';
+    itemRow += '<div class="tcell">';
+    itemRow += '<input class="input short" type="checkbox">';
+    itemRow += '';
+    itemRow += '';
+    itemRow += '';
+    itemRow += '';
+    itemRow += '';
+    itemRow += '';
+    itemRow += '';
+
+
+    // <div class="trow">
+    //     <div class="tcell">
+    //         <input class="input short" type="checkbox" >
+    //     </div>
+    //     <div class="tcell">
+    //         <input class="input short" type="text">
+    //     </div>
+    //     <div class="tcell">
+    //         <input class="input" type="text">
+    //     </div>
+    //     <div class="tcell">
+    //         <select class="input">
+    //             <option value="0.01">Tiny (100 to 1)</option>
+    //             <option value="0.1">Small (10 to 1)</option>
+    //             <option value="1" selected>Normal (1)</option>
+    //             <option value="2">Weighty (2)</option>
+    //             <option value="5">Massive (5)</option>
+    //         </select>
+    //     </div>
+    //     <div class="tcell">
+    //         <input class="input short" type="text" readonly>
+    //     </div>
+    // </div>
+}
 
 function addNewRow(id){
     let div = document.getElementById(id);
