d0ae8ef996d8bc7cdfdcb4a2c810d22d9147bb7e
Matthew Schwencke <matt.schwencke@gmail.com>
2020-10-06 06:50:26 +0900

    Can now add new rows to table.

--- encumbrance.js
+++ encumbrance.js
@@ -1,47 +1,30 @@
-var itemRow;
+var initComplete = false;
+var itemRowHtml;
+var itemRowDiv;
 
 var tableList = [];
 
 function initRow(){
-    itemRow += '<div class="trow">';
-    itemRow += '<div class="tcell">';
-    itemRow += '<input class="input short" type="checkbox">';
-    itemRow += '';
-    itemRow += '';
-    itemRow += '';
-    itemRow += '';
-    itemRow += '';
-    itemRow += '';
-    itemRow += '';
+    itemRowHtml = document.getElementById("row_proto").innerHTML;
+    initComplete = true;
 
-
-    // <div class="trow">
-    //     <div class="tcell">
-    //         <input class="input short" type="checkbox" >
-    //     </div>
-    //     <div class="tcell">
-    //         <input class="input short" type="text">
-    //     </div>
-    //     <div class="tcell">
-    //         <input class="input" type="text">
-    //     </div>
-    //     <div class="tcell">
-    //         <select class="input">
-    //             <option value="0.01">Tiny (100 to 1)</option>
-    //             <option value="0.1">Small (10 to 1)</option>
-    //             <option value="1" selected>Normal (1)</option>
-    //             <option value="2">Weighty (2)</option>
-    //             <option value="5">Massive (5)</option>
-    //         </select>
-    //     </div>
-    //     <div class="tcell">
-    //         <input class="input short" type="text" readonly>
-    //     </div>
-    // </div>
 }
 
-function addNewRow(id){
-    let div = document.getElementById(id);
+function Init(){
+    itemRowHtml = document.getElementById("row_proto").innerHTML;
+    initComplete = true;
+}
 
-    div.innerHTML += itemRow;
+function addNewRow(tableId){
+    
+    let table = document.getElementById(tableId);
+    let newRow = document.createElement("div");
+    newRow.setAttribute("class", "trow");
+    newRow.innerHTML = itemRowHtml;
+    table.append(newRow);
+}
+
+window.onload = function(){
+    itemRowHtml = document.getElementById("row_proto").innerHTML;
+    initComplete = true;
 }
